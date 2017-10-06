c24a4b96543eb6528052d23ed1161130c7499f33
Ivan Melnichuk <melnichuk.cadet@gmail.com>
2017-10-06 15:13:13 +0300

    Added boilerplate code to School

--- src/main/java/hibernate/test/school/School.java
+++ src/main/java/hibernate/test/school/School.java
@@ -1,7 +1,16 @@
 package hibernate.test.school;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class School {
+    private SessionFactory sessionFactory;
+
+    public School() {
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+    }
+
 	public static void main(String[] args) {
-		//TODO write code here
+        //TODO write code here
 	}
 }
